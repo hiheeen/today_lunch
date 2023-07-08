6cb67ceafd9e5bf993763f9a6ba3d5781a442f09
byungyong <quddyddlrk@naver.com>
2023-07-08 17:22:47 +0900

    Place모델 likes_num/hates_num field 삭제 및 Userserializer 수정

--- backend/places/models.py
+++ backend/places/models.py
@@ -6,8 +6,6 @@ class Place(models.Model):
     link = models.CharField(max_length=150)
     image = models.URLField(null=True)
     description = models.TextField(default='증말 맛있어요!')
-    likes_num = models.PositiveIntegerField(default=0)
-    hates_num = models.PositiveIntegerField(default=0) 
     user = models.ForeignKey('users.User',on_delete=models.CASCADE)
     like_user = models.ManyToManyField('users.User',related_name='like_user',blank=True)
     hate_user = models.ManyToManyField('users.User',related_name='hate_user',blank=True)
